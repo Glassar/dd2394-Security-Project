6a66c671a666ac557858361b3b747748ead0ff70
Jonatan <jtuv@kth.se>
2024-10-02 14:10:31 +0200

    Implement e91 decoding

--- e91.py
+++ e91.py
@@ -1,16 +1,21 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit_aer import AerSimulator
 import numpy as np
+import random
 
 simulator = AerSimulator()
 
-n = 32
+n = 16
 
-aliceBasis = np.random.randint(3, size= n)
-bobBasis = np.random.randint(3, size= n)
+aliceBasis = [] 
+bobBasis = []
+
+for i in range(n):
+    aliceBasis.append(random.choice(["X", "Y", "Z"]))
+    bobBasis.append(random.choice(["Y", "Z", "W"]))
 
 
-def send_measure_bits(alice_base, bobs_base):
+def send_qubit(alice_base, bobs_base):
     
     # Charlie generating entangled particles
     qbits = QuantumRegister(2, 'q')
@@ -23,28 +28,28 @@ def send_measure_bits(alice_base, bobs_base):
     bell.cx(qbits[0], 1)
 
     # Alice's base
-    if(alice_base == 0):
+    if(alice_base == "X"):
         bell.h(qbits[0])
         bell.measure(qbits[0], measure[0])
-    elif(alice_base == 1):
+    elif(alice_base == "Y"):
         bell.s(qbits[0])
         bell.h(qbits[0])
         bell.t(qbits[0])
         bell.h(qbits[0])
         bell.measure(qbits[0], measure[0])
-    elif(alice_base == 2):
+    elif(alice_base == "Z"):
         bell.measure(qbits[0], measure[0])
 
     # Bob's base
-    if(bobs_base == 0):
+    if(bobs_base == "Y"):
         bell.s(qbits[1])
         bell.h(qbits[1])
         bell.t(qbits[1])
         bell.h(qbits[1])
         bell.measure(qbits[1], measure[1])
-    elif(bobs_base == 1):
+    elif(bobs_base == "Z"):
         bell.measure(qbits[1], measure[1])
-    elif(bobs_base == 2):
+    elif(bobs_base == "W"):
         bell.measure(qbits[1], measure[1])
         bell.s(qbits[1])
         bell.h(qbits[1])
@@ -54,7 +59,42 @@ def send_measure_bits(alice_base, bobs_base):
 
     t_bell = transpile(bell, simulator)
 
-    return simulator.run(t_bell, shots=1, memory=True).result().get_counts(t_bell)
+    return simulator.run(t_bell, shots=1, memory=True).result().get_memory(t_bell)[0]
 
-for i in range(n):
-    print(send_measure_bits(aliceBasis[i],bobBasis[i]), aliceBasis[i], bobBasis[i])
+
+def measure_all_qubits(aliceBasis, bobBasis):
+
+    alicesMeasurement = []
+    bobsMeasurement = []
+
+    for i in range(n):
+        output = send_qubit(aliceBasis[i],bobBasis[i])
+        alicesMeasurement.append(1 if not int (output[0]) else 0)
+        bobsMeasurement.append(int (output[1]))
+
+    return alicesMeasurement, bobsMeasurement
+
+def sync_bases_and_build_keys(aliceBasis, bobBasis):
+
+    alicesMeasurement, bobsMeasurement = measure_all_qubits(aliceBasis, bobBasis)
+
+    print("Alice: My bases are X, Y, Z")
+    print("Alice: My random basis is:", aliceBasis)
+
+    print("Bob: My bases are Y, Z, W")
+    print("Bob: My random basis is:", bobBasis)
+
+    aliceKey = []
+    bobKey = []
+
+    for i in range(n):
+        if(aliceBasis[i] == bobBasis[i]):
+            aliceKey.append(alicesMeasurement[i])
+            bobKey.append(bobsMeasurement[i])
+
+    return(aliceKey, bobKey)
+
+aliceKey, bobKey = sync_bases_and_build_keys(aliceBasis, bobBasis)
+
+print("Alice's key:", aliceKey)
+print("Bob's key:", bobKey)
